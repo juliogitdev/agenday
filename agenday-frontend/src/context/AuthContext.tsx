94fcf0b0fd8e52981f6bd846a52ad722d535d619
roberto-xz <devv-robert@outlook.com>
2026-05-17 20:06:39 -0300

    feat: add authReady and refreshToken to AuthContextType for improved authentication handling

--- agenday-frontend/src/context/AuthContext.tsx
+++ agenday-frontend/src/context/AuthContext.tsx
@@ -5,6 +5,8 @@ import type { UserLogged, UserLogin, UserSignup } from '../types/User';
 type AuthContextType = {
 	user: UserLogged | null;
 	loginType: string;
+	authReady: boolean;
+	refreshToken: () => Promise<boolean>;
   	login: (userData: UserLogin, loginType: string) => Promise<number>;
   	signup: (userData: UserSignup, loginType: string) => Promise<number>;
   	logout:() => Promise<number>;
@@ -13,6 +15,8 @@ type AuthContextType = {
 const AuthContext = createContext<AuthContextType>({
   	user: null,
 	loginType: 'email',
+	authReady: false,
+	refreshToken: async () => false,
   	login: async () => -0,
 	signup: async () => 0,
 	logout: async () => 0,
