54e541978e97280ef19862bda0022bfe04f288db
roberto-xz <devv-robert@outlook.com>
2026-05-17 20:07:30 -0300

    feat: enhance authentication flow with automatic token refresh and improved login/signup handling

--- agenday-frontend/src/providers/AuthProvider.tsx
+++ agenday-frontend/src/providers/AuthProvider.tsx
@@ -5,94 +5,171 @@ import AuthContext from "../context/AuthContext";
 
 export function AuthProvider({children}: {children: React.ReactNode}) {
 	const [user, setUser] = React.useState<UserLogged | null>(null);
+	const [authReady, setAuthReady] = React.useState(false);
 	const [loginType, setLoginType] = React.useState<string>('email');
+
+	// realiza uma tentativa de refresh automático ao iniciar a aplicação.
+	// authReady evita renderizar rotas protegidas antes da validação da sessão.
+	React.useEffect(() => {
+		const initAuth = async () => {
+			await refreshToken();
+			await new Promise(resolve => setTimeout(resolve, 1000)); // simula delay de carregamento
+			setAuthReady(true);
+		};
+		initAuth();
+	}, []);
+
 
 	const login = async (userData: UserLogin, loginType: string): Promise<number> => {
 		const API_URL = import.meta.env.VITE_API_URL;
 		setLoginType(loginType);
 		
-		if (loginType == 'email') {	
+		// tenta logar com email+senha
+		if (loginType == 'email' && userData.email && userData.password ) {	
 			let response: Response;
 			try {
 				response = await fetch(`${API_URL}auth/login`, {
 					method: 'POST',
+					credentials: "include",
 					headers: { 'Content-Type': 'application/json' },
 					body: JSON.stringify({
 						email:    userData.email, 
 						password: userData.password
 					})
 				});
-			} catch (error) { return 500; /* Internal Server Error */ }
+			} catch (error) { return 500;}
 
 			if (response.ok && response.status == 200 ) {
 				const data = await response.json();
-				console.log(data);
-				setUser({
-					accessToken: data.accessToken,
-					refreshToken: data.refreshToken,
-					type: data.type
-				});
+				setUser({ accessToken: data.accessToken, type: data.type});
 			}
  
 			return response.status;
 		} 
-		
-		if (loginType == 'google') {
+
+		// tenta logar com google
+		if (loginType == 'google' && userData.googleId) {
 			let response: Response;
 			try {
 				response = await fetch(`${API_URL}auth/google`, {
 					method: 'POST',
+					credentials: "include",
 					headers: { 'Content-Type': 'application/json' },
-					body: JSON.stringify({
-						googleToken: userData.googleToken
-					})
+					body: JSON.stringify({idToken: userData.googleId})
 				});
-			} catch (error) { return 500; /* Internal Server Error */ }
+			} catch (error) { return 500;}
 
 			if (response.ok && response.status == 200 ) {
 				const data = await response.json();
-				setUser({
-					accessToken: data.accessToken,
-					refreshToken: data.refreshToken,
-					type: data.type
-				});
+				setUser({ accessToken: data.accessToken, type: data.type});
 			} 
 			return response.status;
 		}
 
-		return 0;
+		return 1;
 	};
+
 
 	const signup = async (userData: UserSignup, loginType: string): Promise<number> => {
 		const API_URL = import.meta.env.VITE_API_URL;
-		if (loginType == 'email') {
+
+		// tenta cadastrar com email+senha
+		if (loginType == 'email' 
+				&& userData.email 
+				&& userData.password
+				&& userData.fullName
+				&& userData.numberPhone
+				&& userData.state
+				&& userData.city
+		) {
 			let response: Response;
 			try {
 				response = await fetch(`${API_URL}auth/register`, {
 					method: 'POST',
 					headers: { 'Content-Type': 'application/json' },
 					body: JSON.stringify({
-						fullName: userData.fullName,
-						email:    userData.email, 
-						password: userData.password,
-						numberPhone: userData.numberPhone,
-						state: userData.state,
-						city: userData.city,
+						fullName: userData.fullName, email:       userData.email, 
+						password: userData.password, numberPhone: userData.numberPhone,
+						state:    userData.state,    city:        userData.city,
 					})
 				});
 			} 
-			catch (error) { return 500; /* Internal Server Error */ } 
+			catch (error) { return 500; }
+			return response.status;
+		} 
+
+		// tenta cadastrar com google
+		else if (loginType == 'google' && userData.googleId) {
+			let response: Response;
+			try {
+				response = await fetch(`${API_URL}auth/google`, {
+					method: 'POST',
+					credentials: "include",
+					headers: { 'Content-Type': 'application/json' },
+					body: JSON.stringify({idToken: userData.googleId})
+				});
+			} catch (error) { return 500;}
+
+			if (response.ok && response.status == 200 ) {
+				const data = await response.json();
+				setUser({ accessToken: data.accessToken, type: data.type});
+			} 
 			return response.status;
 		}
 
-		return 0;
+		return 1;
 	};
 
-	const logout = async(): Promise<number> => { setUser(null); return 100;};
+
+	const refreshToken = async(): Promise<boolean> => { 
+		const API_URL = import.meta.env.VITE_API_URL;
+		try {
+			const response = await fetch(`${API_URL}auth/refresh`, {
+				method: 'POST',
+				credentials: "include",
+			});
+
+			if (response.ok && response.status == 200) {
+				const data = await response.json();
+				setUser({accessToken: data.accessToken, type: data.type});
+				return true;
+			}
+           setUser(null);
+		   return false;
+		}
+		catch (error) { return false; }
+	};
+
+
+	const logout = async(): Promise<number> => { 
+		const API_URL = import.meta.env.VITE_API_URL;
+		try {
+			const response = await fetch(`${API_URL}auth/logout`, {
+				method: 'POST',
+				credentials: "include",
+			});
+
+			if (response.ok && response.status == 200) {
+				setUser(null);
+				return 200;
+			}
+		}
+		catch (error) { return 500; }
+		return 500;
+	};
 	
 	return (
-		<AuthContext.Provider value={{ user, loginType, login, logout, signup }}>
+		<AuthContext.Provider value={{
+			 user, 
+			 loginType, 
+			 login, 
+			 logout, 
+			 signup, 
+			 refreshToken, 
+			 authReady 
+		}}>
 			{ children } 
 		</AuthContext.Provider>
 	);
 }
+
