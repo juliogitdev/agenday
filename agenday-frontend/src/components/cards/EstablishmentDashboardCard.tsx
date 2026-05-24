2375cdef790b0fa6292974e5f9af0e569442ca4a
roberto-xz <devv-robert@outlook.com>
2026-05-24 00:26:55 -0300

    Add EstablishmentDashboardCard component and styles for displaying establishment data

---
+++ agenday-frontend/src/components/cards/EstablishmentDashboardCard.tsx
@@ -0,0 +1,37 @@
+
+
+import type { EstablishmenteDashboardCardType } from "../../types/Estableshment";
+import { BestServicesChart } from "../graphics/BestServicesChart";
+import { CustomerSinceCard } from "./CustomerSinceCard";
+import style from "./styles/establishmentDashboardCard.module.css";
+
+export function EstablishmentDashboardCard({ data }: { data: EstablishmenteDashboardCardType }) {
+    return (
+        <section className={style.establishmentDashboardCard}>
+            <h1 className={style.establishmentDashboardCardTitle}>{data.name}</h1>
+			
+			<div className={style.establishmentDashboardCardInfo}>
+				<div className={style.establishmentDashboardCardInfoItem}>
+					<span className={style.establishmentDashboardCardInfoItemLabel}>receita mensal</span>
+					<span className={style.establishmentDashboardCardInfoItemValue}> R$ {data.mensalAmount}</span>
+				</div>	
+				<div className={style.establishmentDashboardCardInfoItem}>
+					<span className={style.establishmentDashboardCardInfoItemLabel}>serviços por semana</span>
+					<span className={style.establishmentDashboardCardInfoItemValue}>{data.servicesPerWeek}</span>
+				</div>
+			</div>
+			
+
+			<h2 className={style.establishmentDashboardCardTitle}>to 4 clientes</h2>
+			<ul className={style.establishmentDashboardCardTopClients}>
+				<CustomerSinceCard data={data.topClientes[0]} />
+				<CustomerSinceCard data={data.topClientes[1]} />
+				<CustomerSinceCard data={data.topClientes[2]} />
+				<CustomerSinceCard data={data.topClientes[3]} />
+			</ul>
+	
+			<h2 className={style.establishmentDashboardCardTitle}>Serviços mais rentáveis (unidade selecionada)</h2>
+			<BestServicesChart services={data.bestServices} />
+        </section>
+    );
+}
